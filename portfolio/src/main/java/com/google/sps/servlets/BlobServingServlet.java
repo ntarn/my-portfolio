0f33ff084b420b8a235577716e6c1554f9775d84
ntarn <tarn.natalie@gmail.com>
2020-06-15 21:45:34 -0700

    updated blobstore for it to work with live server

---
+++ portfolio/src/main/java/com/google/sps/servlets/BlobServingServlet.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+@WebServlet("/blobstore-serve")
+public class BlobServingServlet extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws IOException {
+            System.out.println("Serve the image");
+            BlobKey blobKey = new BlobKey(req.getParameter("blob-key"));
+            blobstoreService.serve(blobKey, res);
+        }
+}
